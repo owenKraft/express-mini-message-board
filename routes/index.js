2b6977b7249af295314a547a33da60984ffdb6b2
Owen Kraft <owenkraft7@gmail.com>
2021-06-12 19:33:39 -0600

    working unstyled message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', function(req,res,next){
+  res.render('form')
+})
+
+router.post('/new', function(req,res,next){
+  const text = req.body.message
+  const user = req.body.name
+  const added = new Date()
+
+  messages.push({text: text, user: user, added: added})
+  res.redirect('/')
+})
 
 module.exports = router;
